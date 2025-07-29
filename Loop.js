43f01e0e8ff202a52b7796c206d9644843c1a863
Ariful <arifulq234@gmail.com>
2025-07-29 23:45:44 +0600

    first commit

---
+++ Loop.js
@@ -0,0 +1,9 @@
+// const foods = ["polao", "Beef", "Pulse", "Rost", "mutton"];
+// for(const food of foods){
+//     console.log(food)
+// }
+const foods = ["polao", "Beef", "Pulse", "Rost", "mutton"];
+for(const food of foods){
+    console.log(food)
+    console.log("I Want to eat")
+}
