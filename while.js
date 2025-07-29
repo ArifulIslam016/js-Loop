70265441673912088bdde7c7a9fdeed219af572a
Ariful <arifulq234@gmail.com>
2025-07-30 00:31:58 +0600

    Problem solving by while loop

--- while.js
+++ while.js
@@ -5,3 +5,14 @@ while (number < 10) {
   number++;
   console.log("this is number 1:",number);
 }
+
+// Another While of message print
+let messageNum=0;
+
+while(messageNum<=60){
+    messageNum++
+    console.log(
+      "I will invest at least 6 hrs every single day for next 60 days!"
+    );
+}
+
