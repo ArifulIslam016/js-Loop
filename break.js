fe6a34d27785b3edb07c2655502179307a209513
Ariful <arifulq234@gmail.com>
2025-07-31 23:56:05 +0600

    Exploring many kind of arrays

---
+++ break.js
@@ -0,0 +1,24 @@
+let fruits=["mango","Banana", "apple", "Pinapple"]
+// console.log(fruits.includes("Banana"))
+// console.log(fruits[2])
+// console.log(fruits.indexOf("apple"))
+
+
+for(let n=0;n<=12;n++){
+  
+    if(n===10){
+    break;
+}
+  console.log(n);
+}
+
+// for(let n=0;n<=12;n++){
+//        if(n===10){
+//     continue;
+// }
+//     console.log(n)
+
+// }
+
+
+
